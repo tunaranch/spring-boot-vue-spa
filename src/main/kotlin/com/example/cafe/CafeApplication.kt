1f5ced7d05a208fd9aeaf7272040f552aa3d2eff
tunaranch <140396+tunaranch@users.noreply.github.com>
2019-03-31 13:26:17 +1000

    Create new Spring Boot project
    Created with:
    
    http https://start.spring.io/starter.tgz \
     artifactId==cafe \
     javaVersion==11 \
     language==kotlin \
     name==Cafe \
     dependencies==webflux,devtools,actuator \
     baseDir==cafe | tar -xzvf -

---
+++ src/main/kotlin/com/example/cafe/CafeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cafe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CafeApplication>(*args)
+}
